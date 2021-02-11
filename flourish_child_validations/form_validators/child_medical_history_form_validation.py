2a17a843a9c1687d05b9f020006c9c65da342f11
ThatoKgamaetsile <thatokgamaetsile@gmail.com>
2021-02-11 15:30:22 +0200

    Add child medical history validator

---
+++ flourish_child_validations/form_validators/child_medical_history_form_validation.py
@@ -0,0 +1,12 @@
+from edc_constants.constants import YES, OTHER
+from edc_form_validators import FormValidator
+
+
+class ChildMedicalHistoryFormValidator(FormValidator):
+
+    def clean(self):
+
+        self.required_if(
+            YES,
+            field='chronic_since',
+            field_required='child_chronic',)
