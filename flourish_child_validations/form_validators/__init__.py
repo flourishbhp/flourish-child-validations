49281e79354cff35b511bed288a067497315c389
ThatoKgamaetsile <thatokgamaetsile@gmail.com>
2021-03-02 20:39:24 +0200

    Add Birth exam form validator

--- flourish_child_validations/form_validators/__init__.py
+++ flourish_child_validations/form_validators/__init__.py
@@ -3,6 +3,6 @@ from .child_assent_form_validator import ChildAssentFormValidator
 from .child_medical_history_form_validator import ChildMedicalHistoryFormValidator
 from .child_referral_form_validator import ChildReferralFormValidator
 from .form_validator_mixin import ChildFormValidatorMixin
-# from .birth_exam_form_validation import BirthExamFormValidator
+from .birth_exam_form_validation import BirthExamFormValidator
 from .infant_arv_exposure_form_validation import InfantArvExposureFormValidator
 from .infant_congenital_anomalies_form_validation import *
