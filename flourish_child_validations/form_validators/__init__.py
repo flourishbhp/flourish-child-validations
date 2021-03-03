96d5444a0ee05e3a508f6e84f5e24663f8599921
ame diphoko <diphokoame@gmail.com>
2021-03-03 09:42:42 +0200

    add socio demographic form validation

--- flourish_child_validations/form_validators/__init__.py
+++ flourish_child_validations/form_validators/__init__.py
@@ -2,6 +2,7 @@ from .birth_data_form_validation import BirthDataFormValidator
 from .child_assent_form_validator import ChildAssentFormValidator
 from .child_medical_history_form_validator import ChildMedicalHistoryFormValidator
 from .child_referral_form_validator import ChildReferralFormValidator
+from .child_socio_demographic_form_validator import ChildSocioDemographicFormValidator
 from .form_validator_mixin import ChildFormValidatorMixin
 # from .birth_exam_form_validation import BirthExamFormValidator
 from .infant_arv_exposure_form_validation import InfantArvExposureFormValidator
