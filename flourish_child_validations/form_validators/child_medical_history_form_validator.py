5742abe85c88d323a5090b46d35a942d65768845
tumiemosweu <imosweuk@gmail.com>
2021-04-07 17:22:07 +0200

    update medical history validations

--- flourish_child_validations/form_validators/child_medical_history_form_validator.py
+++ flourish_child_validations/form_validators/child_medical_history_form_validator.py
@@ -1,6 +1,6 @@
 from django.core.exceptions import ValidationError
 
-from edc_constants.constants import YES, NO, OTHER, NOT_APPLICABLE
+from edc_constants.constants import YES, NO, OTHER, NOT_APPLICABLE, NONE
 from edc_form_validators import FormValidator
 
 
@@ -18,23 +18,25 @@ class ChildMedicalHistoryFormValidator(FormValidator):
             m2m_field='child_chronic',
             field_other='child_chronic_other')
 
+        self.m2m_single_selection_if(NONE, m2m_field='who')
+
         self.not_applicable_not_allowed(NOT_APPLICABLE, field=chronic_since,
                                         m2m_field=child_chronic)
 
     def not_applicable_not_allowed(self, *selections, field=None, m2m_field=None):
 
-        selected = {obj.short_name: obj.name for obj in m2m_field}
-        if field == YES and m2m_field:
-            for selection in selections:
-                if selection in selected:
+        if m2m_field and field:
+            selected = {obj.short_name: obj.name for obj in m2m_field if m2m_field is not None}
+            if field == YES and m2m_field:
+                for selection in selections:
+                    if selection in selected:
+                        message = {'child_chronic':
+                                   'This field is applicable'}
+                        self._errors.update(message)
+                        raise ValidationError(message)
+            elif field == NO:
+                if NOT_APPLICABLE not in selected:
                     message = {'child_chronic':
-                               'This field is applicable'}
+                               'You can only select \'Not Applicable\''}
                     self._errors.update(message)
                     raise ValidationError(message)
-
-        if field == NO:
-            if NOT_APPLICABLE not in selected:
-                message = {'child_chronic':
-                           'You can only select \'Not Applicable\''}
-                self._errors.update(message)
-                raise ValidationError(message)
