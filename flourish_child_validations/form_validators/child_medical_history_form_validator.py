287a7b566f7fa56a5a8a28c6cc721696023861b5
ame diphoko <diphokoame@gmail.com>
2021-05-18 13:12:31 +0200

    update child medical history form validations

--- flourish_child_validations/form_validators/child_medical_history_form_validator.py
+++ flourish_child_validations/form_validators/child_medical_history_form_validator.py
@@ -11,43 +11,17 @@ class ChildMedicalHistoryFormValidator(FormValidator):
         chronic_since = self.cleaned_data.get('chronic_since')
         child_chronic = self.cleaned_data.get('child_chronic')
 
-        med_history_changed = self.cleaned_data.get('med_history_changed')
-        self.validate_med_history_changed(med_history_changed)
-        if not med_history_changed or med_history_changed == YES:
-            self.m2m_single_selection_if(NOT_APPLICABLE, m2m_field='child_chronic')
+        self.m2m_single_selection_if(NOT_APPLICABLE, m2m_field='child_chronic')
 
-            self.m2m_other_specify(
-                OTHER,
-                m2m_field='child_chronic',
-                field_other='child_chronic_other')
+        self.m2m_other_specify(
+            OTHER,
+            m2m_field='child_chronic',
+            field_other='child_chronic_other')
 
-            self.m2m_single_selection_if(NONE, m2m_field='who')
+        self.m2m_single_selection_if(NONE, m2m_field='who')
 
-            self.not_applicable_not_allowed(NOT_APPLICABLE, field=chronic_since,
-                                            m2m_field=child_chronic)
-
-    def validate_med_history_changed(self, med_history_changed):
-        if med_history_changed:
-            if med_history_changed == NOT_APPLICABLE:
-                msg = {'med_history_changed': 'This field is applicable.'}
-                self._errors.update(msg)
-                raise ValidationError(msg)
-
-            self.not_applicable_if(
-                NO,
-                field='med_history_changed',
-                field_applicable='chronic_since')
-
-            self.not_required_if(
-                NO,
-                field='med_history_changed',
-                field_required='child_chronic_other',
-                inverse=False)
-
-            if med_history_changed == NO:
-                m2m_fields = ['child_chronic', 'who']
-                for field in m2m_fields:
-                    self.validate_m2m_na(field)
+        self.not_applicable_not_allowed(NOT_APPLICABLE, field=chronic_since,
+                                        m2m_field=child_chronic)
 
     def not_applicable_not_allowed(self, *selections, field=None, m2m_field=None):
 
