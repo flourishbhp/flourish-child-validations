7fa34d3cf5c34e611f4deb6ce80baf2122138a6a
Itumeleng Mosweu <imosweuk@gmail.com>
2022-10-31 15:16:23 +0200

    update child referral validations

--- flourish_child_validations/form_validators/child_referral_form_validator.py
+++ flourish_child_validations/form_validators/child_referral_form_validator.py
@@ -1,4 +1,6 @@
+from edc_constants.constants import NO, YES
 from edc_form_validators import FormValidator
+
 from .form_validator_mixin import ChildFormValidatorMixin
 
 
@@ -10,4 +12,37 @@ class ChildReferralFormValidator(ChildFormValidatorMixin, FormValidator):
 
         self.validate_consent_version_obj(self.subject_identifier)
 
-        self.validate_other_specify(field='referred_to')
+        other_fields = ['referred_to', 'support_ref_decline_reason',
+                        'no_support_reason', 'emo_support_type', 'emo_health_improved',
+                        'percieve_counselor']
+
+        for other_field in other_fields:
+            self.validate_other_specify(field=other_field)
+
+        self.required_if(NO,
+                         field='attended_referral',
+                         field_required='support_ref_decline_reason')
+
+        self.required_if(YES,
+                         field='attended_referral',
+                         field_required='emo_support')
+
+        self.required_if(NO,
+                         field='emo_support',
+                         field_required='no_support_reason')
+
+        self.m2m_required_if(YES,
+                             field='emo_support',
+                             m2m_field='emo_support_type')
+
+        emo_fields = ['emo_health_improved', 'percieve_counselor',
+                      'satisfied_counselor']
+
+        for emo in emo_fields:
+            self.required_if(YES,
+                             field='emo_support',
+                             field_required=emo)
+
+        self.required_if(NO,
+                         field='satisfied_counselor',
+                         field_required='additional_counseling')
