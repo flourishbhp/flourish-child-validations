973f70b72d82552a32ca239fc565c23d2c9ff31a
Moses Chawawa <moseschawawa@gmail.com>
2022-12-07 14:37:30 +0200

    Validations fix for tb history

--- flourish_child_validations/form_validators/tb_adol_validations/tb_history_form_validator.py
+++ flourish_child_validations/form_validators/tb_adol_validations/tb_history_form_validator.py
@@ -11,26 +11,17 @@ class TbHistoryFormValidator(ChildFormValidatorMixin, FormValidator):
         
         self.validate_history_of_tbt_required_fields()
         
-        self.validate_other_specify(
-            field='reason_for_therapy',
-            other_specify_field='reason_for_therapy_other'
-        )
+        # self.validate_other_specify(
+        #     field='reason_for_therapy',
+        #     other_specify_field='reason_for_therapy_other' 
+        # )
+    
+        # for field in ['tb_diagnosis_type', 'prior_treatmnt_history']:
+        #     self.required_if(YES,
+        #                     field='prior_tb_history',
+        #                     field_required=field)
         
-        self.required_if(YES,
-                         field='prior_tb_history',
-                         field_required='tb_diagnosis_type')
-        
-        self.required_if('outside_the_lungs', 'both',
-                         field='tb_diagnosis_type',
-                         field_required='extra_pulmonary_loc')
-        self.validate_other_specify(
-            field='extra_pulmonary_loc',
-            other_specify_field='other_loc'
-        )
-        
-        self.not_required_if('outside_the_lungs', 'both',
-                             field='tb_diagnosis_type',
-                             field_required='prior_treatmnt_history')
+
         
         self.validate_prior_tb_history()
         
@@ -44,12 +35,31 @@ class TbHistoryFormValidator(ChildFormValidatorMixin, FormValidator):
                              field='history_of_tbt',
                              field_required=field)
             
+        self.validate_other_specify(
+            field='reason_for_therapy',
+            other_specify_field='reason_for_therapy_other' 
+        )
+    
+            
     def validate_prior_tb_history(self):
-        fields = ['tb_drugs_freq', 'iv_meds_used', 'tb_treatmnt_completed']
+        fields = ['tb_diagnosis_type','tb_drugs_freq', 'iv_meds_used', 'tb_treatmnt_completed']
         
         for field in fields:
             self.required_if(
                 YES,
-                field='prior_treatmnt_history',
+                field='prior_tb_history',
                 field_required=field
             )
+            
+        self.required_if('outside_the_lungs', 'both',
+                         field='tb_diagnosis_type',
+                         field_required='extra_pulmonary_loc')
+        
+        self.validate_other_specify(
+            field='extra_pulmonary_loc',
+            other_specify_field='other_loc'
+        )
+        
+        self.not_required_if('outside_the_lungs', 'both', None,
+                             field='tb_diagnosis_type',
+                             field_required='prior_treatmnt_history')
