ef50194f5328c7c7b059ed26c1e9ac6257487263
Ame Diphoko <diphokoame@gmail.com>
2023-11-16 13:21:08 +0200

    fix: refactor social work referral validator mixin

--- flourish_child_validations/form_validators/child_social_work_referral_form_validator.py
+++ flourish_child_validations/form_validators/child_social_work_referral_form_validator.py
@@ -1,13 +1,33 @@
-from edc_form_validators import FormValidator
-from flourish_form_validations.form_validators.caregiver_social_work_referral_form_validator import CaregiverSocialWorkReferralFormValidator
+from flourish_form_validations.form_validators import SocialWorkReferralValidatorMixin
+
 from .form_validator_mixin import ChildFormValidatorMixin
 
 
 class ChildSocialWorkReferralValidator(ChildFormValidatorMixin,
-                                       CaregiverSocialWorkReferralFormValidator, FormValidator):
+                                       SocialWorkReferralValidatorMixin):
 
     def clean(self):
         self.subject_identifier = self.cleaned_data.get(
             'child_visit').appointment.subject_identifier
 
         super().clean()
+
+    def validate_referral_reason(self):
+        self.m2m_other_specify('refer_c_other',
+                               m2m_field='referral_reason',
+                               field_other='reason_other')
+
+        referral_reason = self.cleaned_data.get('referral_reason')
+        selected = [reason.short_name for reason in referral_reason]
+        value_field = {'local_medical_facility': 'comment', }
+
+        for value, field in value_field.items():
+            if isinstance(field, list):
+                for required in field:
+                    self.required_if_true(
+                        value in selected,
+                        field_required=required)
+            else:
+                self.required_if_true(
+                    value in selected,
+                    field_required=field)
