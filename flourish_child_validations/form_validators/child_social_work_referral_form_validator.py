de96296c4752289815044cd5e2a4bc4e49cedaa7
Keba462 <79043038+Keba462@users.noreply.github.com>
2023-11-28 15:28:51 +0200

    Refactor validations

--- flourish_child_validations/form_validators/child_social_work_referral_form_validator.py
+++ flourish_child_validations/form_validators/child_social_work_referral_form_validator.py
@@ -11,3 +11,23 @@ class ChildSocialWorkReferralValidator(ChildFormValidatorMixin,
             'child_visit').appointment.subject_identifier
 
         super().clean()
+
+    def validate_referral_reason(self):
+        self.m2m_other_specify('refer_c_other',
+                               m2m_field='referral_reason',
+                               field_other='reason_other')
+
+        referral_reason = self.cleaned_data.get('referral_reason', [])
+        selected = [reason.short_name for reason in referral_reason]
+        value_field = {'local_medical_facility': 'comment', }
+
+        for value, field in value_field.items():
+            if isinstance(field, list):
+                for required in field:
+                    self.required_if_true(
+                        value in selected,
+                        field_required=required)
+            else:
+                self.required_if_true(
+                    value in selected,
+                    field_required=field)
