0d48f603b3829044e6f33dea213a9a1aed5270c4
Ame Diphoko <diphokoame@gmail.com>
2024-03-20 10:57:38 +0200

    add: validations for gender, pregnancy and exposure_status; referral form

--- flourish_child_validations/form_validators/child_social_work_referral_form_validator.py
+++ flourish_child_validations/form_validators/child_social_work_referral_form_validator.py
@@ -1,5 +1,8 @@
+from django.core.exceptions import ValidationError
+from edc_constants.constants import YES, FEMALE
 from flourish_form_validations.form_validators.social_work_referral_validator_mixin \
     import SocialWorkReferralValidatorMixin
+
 from .form_validator_mixin import ChildFormValidatorMixin
 
 
@@ -7,10 +10,9 @@ class ChildSocialWorkReferralValidator(ChildFormValidatorMixin,
                                        SocialWorkReferralValidatorMixin):
 
     def clean(self):
-        self.subject_identifier = self.cleaned_data.get(
-            'child_visit').appointment.subject_identifier
-
         super().clean()
+        self.validate_is_preg()
+        self.validate_child_exposure_status()
 
     def validate_referral_reason(self):
         self.m2m_other_specify('refer_c_other',
@@ -31,3 +33,23 @@ class ChildSocialWorkReferralValidator(ChildFormValidatorMixin,
                 self.required_if_true(
                     value in selected,
                     field_required=field)
+
+    def validate_is_preg(self):
+        child_gender = getattr(
+            self.child_consent_model_obj, 'gender', None)
+        self.required_if_true(
+            child_gender == FEMALE,
+            field_required='is_preg',
+            not_required_msg='Participant is Male, this field is not required')
+
+    def validate_child_exposure_status(self):
+        exposure_status = self.cleaned_data.get('child_exposure_status', None)
+        cohort_exposure = self.cohort_model_obj().check_exposure()
+        child_exposed = cohort_exposure == 'EXPOSED'
+        child_unexposed =  cohort_exposure == 'UNEXPOSED'
+        if (child_exposed and exposure_status != 'heu'):
+            raise ValidationError(
+                {'child_exposure_status': 'This child is HIV exposed, please correct. '})
+        elif child_unexposed and exposure_status != 'huu':
+            raise ValidationError(
+                {'child_exposure_status': 'This child HIV unexposed, please correct. '})
