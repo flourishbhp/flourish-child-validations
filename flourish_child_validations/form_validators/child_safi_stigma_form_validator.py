e8fe96d6bf021409a6d5b845f56c3659171b388b
Keba462 <79043038+Keba462@users.noreply.github.com>
2024-04-30 10:06:21 +0200

    Update child_safi_stigma_form_validator.py

--- flourish_child_validations/form_validators/child_safi_stigma_form_validator.py
+++ flourish_child_validations/form_validators/child_safi_stigma_form_validator.py
@@ -31,6 +31,18 @@ class ChildSafiStigmaFormValidator(ChildFormValidatorMixin, FormValidator):
                 field=field,
                 field_required=f'{field}_period'
             )
+        discriminated = any(
+            [self.cleaned_data.get(field, None) == 'ever_happened' for field in fields])
+        discriminated_at_other = bool(
+            self.cleaned_data.get('other_place_discr', None))
+        for field in fields:
+            self.applicable_if_true(
+                discriminated or discriminated_at_other,
+                field_applicable=field,
+                applicable_msg=(
+                    'This field is applicable, participant experienced discrimination'),
+                not_applicable_msg=(
+                    'This field is not applicable, no discrimination was experienced.'))
 
         self.required_if_not_none(
             field='other_place_discr',
