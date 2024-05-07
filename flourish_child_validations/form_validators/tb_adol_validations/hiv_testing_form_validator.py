a11aeb60ffd1ddf52cb59fbfcbe6538ceaea9da6
Ame Diphoko <diphokoame@gmail.com>
2024-05-07 15:26:14 +0200

    fix pep8

--- flourish_child_validations/form_validators/tb_adol_validations/hiv_testing_form_validator.py
+++ flourish_child_validations/form_validators/tb_adol_validations/hiv_testing_form_validator.py
@@ -2,9 +2,9 @@ from edc_form_validators import FormValidator
 from edc_constants.constants import YES, POS
 from ..form_validator_mixin import ChildFormValidatorMixin
 
+
 class HIVTestingFormValidator(ChildFormValidatorMixin, FormValidator):
-      
-      
+
     def clean(self):
         super().clean()
         '''
@@ -16,25 +16,25 @@ class HIVTestingFormValidator(ChildFormValidatorMixin, FormValidator):
         'date_initiated_treatment',
         'seen_by_healthcare',
         '''
-        
+
         hiv_required_fields = ['times_tested', 'last_result']
-        
+
         for field in hiv_required_fields:
             self.required_if(YES,
-                            field='test_for_hiv',
-                            field_required=field)
-        
+                             field='test_for_hiv',
+                             field_required=field)
+
         fields = [
             'referred_for_treatment',
             'initiated_treatment',
             'seen_by_healthcare',
         ]
-   
-        for field in fields:   
+
+        for field in fields:
             self.required_if(POS,
-                            field='last_result',
-                            field_required=field)
-   
+                             field='last_result',
+                             field_required=field)
+
         self.required_if(YES,
                          field='initiated_treatment',
                          field_required='date_initiated_treatment')
--- flourish_child_validations/form_validators/tb_adol_validations/tb_visit_screening_validator.py
+++ flourish_child_validations/form_validators/tb_adol_validations/tb_visit_screening_validator.py
@@ -3,15 +3,15 @@ from edc_constants.constants import YES
 from ..form_validator_mixin import ChildFormValidatorMixin
 
 
-class TbVisitScreeningFormValidator(ChildFormValidatorMixin, FormValidator):   
-      
+class TbVisitScreeningFormValidator(ChildFormValidatorMixin, FormValidator):
+
     def clean(self):
         super().clean()
 
         self.required_if(YES,
                          field='have_cough',
                          field_required='cough_duration')
- 
+
         self.required_if(YES,
                          field='fever',
                          field_required='fever_duration')
