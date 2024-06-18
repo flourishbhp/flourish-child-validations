725ec787a2141351adfa4acecd95899b72b00252
Ame Diphoko <diphokoame@gmail.com>
2024-06-18 08:53:32 +0200

    update: lmp date required if menarche provided, or pregnant

--- flourish_child_validations/form_validators/child_preg_testing_form_validator.py
+++ flourish_child_validations/form_validators/child_preg_testing_form_validator.py
@@ -11,6 +11,7 @@ from .form_validator_mixin import ChildFormValidatorMixin
 class ChildPregTestingFormValidator(ChildFormValidatorMixin, FormValidator):
 
     caregiver_child_consent_model = 'flourish_caregiver.caregiverchildconsent'
+    child_preg_testing_model = 'flourish_child.childpregtesting'
 
     def clean(self):
 
@@ -31,9 +32,7 @@ class ChildPregTestingFormValidator(ChildFormValidatorMixin, FormValidator):
                            field='menarche',
                            field_applicable='experienced_pregnancy')
 
-        self.required_if(YES,
-                         field='experienced_pregnancy',
-                         field_required='last_menstrual_period')
+        self.validate_lmp_required()
 
         self.required_if_not_none(field='last_menstrual_period',
                                   field_required='is_lmp_date_estimated')
@@ -52,6 +51,10 @@ class ChildPregTestingFormValidator(ChildFormValidatorMixin, FormValidator):
         return django_apps.get_model(self.caregiver_child_consent_model)
 
     @property
+    def child_preg_testing_model_cls(self):
+        return django_apps.get_model(self.child_preg_testing_model)
+
+    @property
     def caregiver_child_obj(self):
 
         caregiver_chld_consents = self.child_caregiver_consent_model_cls.objects.filter(
@@ -59,6 +62,25 @@ class ChildPregTestingFormValidator(ChildFormValidatorMixin, FormValidator):
 
         if caregiver_chld_consents:
             return caregiver_chld_consents.latest('consent_datetime')
+
+    def validate_lmp_required(self):
+        """ Require lmp date if participant is pregnant, or has reached menarche and it
+            is not the first time.
+        """
+        experienced_preg = self.cleaned_data.get('experienced_pregnancy', '') == YES
+        menarche_dt = self.cleaned_data.get('menarche_start_dt', None)
+        prev_menarche_dt = self.check_prev_menarche_dt
+
+        self.required_if_true(
+            experienced_preg or (menarche_dt and prev_menarche_dt),
+            field_required='last_menstrual_period', )
+
+        if prev_menarche_dt and menarche_dt != prev_menarche_dt:
+            message = {'menarche_start_dt':
+                       f'Previous menarche start date is {prev_menarche_dt}. '
+                       'Date provided does not match this, please correct.'}
+            self._errors.update(message)
+            raise ValidationError(message)
 
     def validate_lmp(self):
         """A function to validate the lmp if it is below the 2months threshold
@@ -90,3 +112,14 @@ class ChildPregTestingFormValidator(ChildFormValidatorMixin, FormValidator):
                 field_applicable='test_done',
                 applicable_msg='A pregnancy test is needed')
 
+    @property
+    def check_prev_menarche_dt(self):
+        child_visit = self.cleaned_data.get('child_visit', None)
+        previous_visit = child_visit.previous_visit
+        try:
+            child_preg_testing = self.child_preg_testing_model_cls.objects.get(
+                child_visit=previous_visit)
+        except self.child_preg_testing_model.DoesNotExist:
+            return None
+        else:
+            return child_preg_testing.menarche_start_dt
