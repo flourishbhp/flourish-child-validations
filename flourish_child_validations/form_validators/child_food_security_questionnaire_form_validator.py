32881d42d7124525d363c570dcbebadccf3cd37a
Ame Diphoko <diphokoame@gmail.com>
2025-01-23 10:08:35 +0200

    add: validations for food security questionnaire, new questions

--- flourish_child_validations/form_validators/child_food_security_questionnaire_form_validator.py
+++ flourish_child_validations/form_validators/child_food_security_questionnaire_form_validator.py
@@ -1,5 +1,7 @@
+from django.core.exceptions import ValidationError
 from edc_constants.constants import YES
 from edc_form_validators import FormValidator
+
 from .form_validator_mixin import ChildFormValidatorMixin
 
 
@@ -16,3 +18,24 @@ class ChildFoodSecurityQuestionnaireFormValidator(ChildFormValidatorMixin, FormV
         self.required_if(YES,
                          field='cut_meals',
                          field_required='how_often')
+
+        respondent = self.cleaned_data.get('answerer')
+        condition = (respondent == 'child_adolescent' and self.child_age < 10)
+        _message = 'Child/Adolescent cannot answer, they are less than 10 years.'
+
+        self.raise_on_selection_condition(condition,
+                                          'answerer',
+                                          _message)
+
+        self.required_if(
+            'both',
+            field='answerer',
+            field_required='both_respondents_details',
+            required_msg=('Details on which questions were responded by who, '
+                          'are required if `Both` was selected.'))
+
+    def raise_on_selection_condition(self, condition, field, message):
+        if condition:
+            msg = {field: message}
+            self._errors.update(msg)
+            raise ValidationError(msg)
